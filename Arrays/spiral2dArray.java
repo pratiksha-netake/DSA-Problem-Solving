e06f0a82ea25c890d70c1de05b945a38aeb20bb9
pratiksha-netake <pratikshanetake11@gmail.com>
2026-02-08 19:31:20 +0530

    2d spiral

---
+++ Arrays/spiral2dArray.java
@@ -0,0 +1,50 @@
+public class spiral2dArray{
+        public static void main(String[] args){
+                int[][] arr=new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+
+                int i=0;
+                int j=0;
+                int n=arr.length;
+                for(int x=0; x<n-1; x++){
+                        System.out.print(arr[i][j]+" ");
+                        j++;
+                }
+
+                for(int x=0; x<n-1; x++){
+                        System.out.print(arr[i][j]+" ");
+                        i++;
+                }
+
+                for(int x=0; x<n-1; x++){
+                        System.out.print(arr[i][j]+" ");
+                        j--;
+                }
+                for(int x=0; x<n-1; x++){
+                        System.out.print(arr[i][j]+" ");
+                        i--;
+                }
+
+		i=1; 
+		j=1;
+		for(int x=0; x<n-3; x++){
+			System.out.print(arr[i][j]+" ");
+			j++;
+		}
+		for(int x=0; x<n-3; x++){
+			System.out.print(arr[i][j]+" ");
+			i++;
+		}
+
+		for(int x=0; x<n-3; x++){
+			System.out.print(arr[i][j]+" ");
+			j--;
+		}
+		for(int x=0; x<n-3; x++){
+			System.out.print(arr[i][j]+" ");
+			i--;
+		}
+
+		
+        }
+}
+
