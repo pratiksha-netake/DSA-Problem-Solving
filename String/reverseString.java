4a9db5ffd96fda55eab8cd0f08b96ecad54e5d53
pratiksha-netake <pratikshanetake11@gmail.com>
2026-04-01 12:41:29 +0530

    Reverse String

---
+++ String/reverseString.java
@@ -0,0 +1,10 @@
+class ReverseString{
+	public static void main(String[] args){
+	String str="pratiksha";
+	StringBuffer sb=new StringBuffer(str);
+	String str2=sb.reverse().toString();
+	System.out.println(str2);
+	}
+}
+
+
