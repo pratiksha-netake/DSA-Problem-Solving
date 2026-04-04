79e71ff75aa630ba5befb53b9f4663f2846cb2f6
pratiksha-netake <pratikshanetake11@gmail.com>
2026-04-04 17:33:11 +0530

    length of string without using length()

---
+++ String/lenOfStr.java
@@ -0,0 +1,12 @@
+class LenOfStr{
+	public static void main(String[] args){
+		String str="pratiksha";
+
+		char[] arr=str.toCharArray();
+                int count=0;
+		for(int i=0; i<arr.length; i++){
+			 count++;
+		}
+		System.out.println("length of string"+count);
+	}
+}
