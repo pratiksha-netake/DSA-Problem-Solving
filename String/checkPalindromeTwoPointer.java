b3fd1fb66f6692b19c605c95e054161a2d6a3bc1
pratiksha-netake <pratikshanetake11@gmail.com>
2026-04-04 17:45:45 +0530

    to check string is palindrome or not using two pointer approach

---
+++ String/checkPalindromeTwoPointer.java
@@ -0,0 +1,26 @@
+class checkPalindrome{
+	public static boolean isPalindrome(String s){
+		int left=0; 
+		int right=s.length()-1;
+
+		while(left<right){
+			if(s.charAt(left)!=s.charAt(right)){
+				return false;
+			}
+			left++;
+			right--;
+		}
+		return true;
+	}
+
+	public static void main(String[] args){
+		String str="pratiksha";
+		boolean result=isPalindrome(str);
+		if(result){
+			System.out.println("given String is palindrome");
+		}else{
+			System.out.println("is not palindrome");
+		}
+	}
+}
+
